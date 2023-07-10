807e40cb25e60773b92e60bdd383a7c0bea7f2ed
saahilpoojary <136162151+saahilpoojary@users.noreply.github.com>
2023-07-10 14:18:55 +0530

    Semphore.c

--- Semphore.c
+++ Semphore.c
@@ -1,116 +1,75 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-#include<stdlib.h>
-
-int mutex=1,full=0,empty=3,x=0;
-
-int main()
-
-{
-
-int n;
-
-void producer();
-
-void consumer();
+int mutex = 1, full = 0, empty = 3, x = 0;
 
 int wait(int);
-
 int signal(int);
+void producer();
+void consumer();
 
-printf("\n1.Producer\n2.Consumer\n3.Exit");
-
-while(1)
-
+int main()
 {
+    int n;
 
-printf("\nEnter your choice:");
+    printf("\n1. Producer\n2. Consumer\n3. Exit");
 
-scanf("%d",&n);
+    while (1)
+    {
+        printf("\nEnter your choice: ");
+        scanf("%d", &n);
 
-switch(n)
+        switch (n)
+        {
+            case 1:
+                if (mutex == 1 && empty != 0)
+                    producer();
+                else
+                    printf("Buffer is full!!");
+                break;
 
-{
+            case 2:
+                if (mutex == 1 && full != 0)
+                    consumer();
+                else
+                    printf("Buffer is empty!!");
+                break;
 
-case 1: if((mutex==1)&&(empty!=0))
+            case 3:
+                exit(0);
+                break;
+        }
+    }
 
-producer();
-
-else
-
-printf("Buffer is full!!");
-
-break;
-
-case 2: if((mutex==1)&&(full!=0))
-
-consumer();
-
-else
-
-printf("Buffer is empty!!");
-
-break;
-
-case 3:
-
-exit(0);
-
-break;
-
-}
-
-}
-
-return 0;
-
+    return 0;
 }
 
 int wait(int s)
-
 {
+    return (--s);
+}
 
-return (--s);
 int signal(int s)
-
 {
-
-return(++s);
-
+    return (++s);
 }
 
 void producer()
-
 {
-
-mutex=wait(mutex);
-
-full=signal(full);
-
-empty=wait(empty);
-
-x++;
-
-printf("\nProducer produces the item %d",x);
-
-mutex=signal(mutex);
-
+    mutex = wait(mutex);
+    full = signal(full);
+    empty = wait(empty);
+    x++;
+    printf("\nProducer produces the item %d", x);
+    mutex = signal(mutex);
 }
 
 void consumer()
-
 {
-
-mutex=wait(mutex);
-
-full=wait(full);
-
-empty=signal(empty);
-
-printf("\nConsumer consumes item %d",x);
-
-x--;
-
-mutex=signal(mutex);
-
+    mutex = wait(mutex);
+    full = wait(full);
+    empty = signal(empty);
+    printf("\nConsumer consumes item %d", x);
+    x--;
+    mutex = signal(mutex);
 }
