e91d038e645f1a052d8be04253956225f4ed6984
saahilpoojary <136162151+saahilpoojary@users.noreply.github.com>
2023-07-10 15:23:54 +0530

    2c.c

--- 2c.c
+++ 2c.c
@@ -15,20 +15,3 @@ int main()
     forkexample();
     return 0;
 }
-/* output
-1. Producer
-2. Consumer
-3. Exit
-Enter your choice: 1
-
-Producer produces the item 1
-Enter your choice: 2
-
-Consumer consumes item 1
-Enter your choice: 2
-Buffer is empty!!
-Enter your choice: 1
-
-Producer produces the item 1
-Enter your choice: 
-*/
