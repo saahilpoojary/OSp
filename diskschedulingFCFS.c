742e29ce00ac8069787867b8a1cab2b3ca299145
saahilpoojary <136162151+saahilpoojary@users.noreply.github.com>
2023-07-31 14:27:53 +0530

    diskschedulingFCFS.c

---
+++ diskschedulingFCFS.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int i,req[50],mov=0,cp,n;
+    printf("Enter current position\n");
+    scanf("%d",&cp);
+    printf("Enter number of requests\n");
+    scanf("%d",&n);
+    printf("Enter the request order\n");
+    for(i=1;i<n;i++)
+    {
+        scanf("%d",&req[i]);
+    }
+    mov=mov+abs(cp-req[0]);
+    printf("%d->%d",cp,req[0]);
+for(i=1;i<n;i++)
+{
+
+    mov=mov+abs(req[i]-req[i-1]);
+    printf("->%d",req[i]);
+
+}
+printf("\n");
+printf("Total movement=%d\n",mov);
+}
